b4a212b8ca07e7cfb84447b6446004f2887eec51
Henry Waill <hwaill@optonline.net>
2020-06-01 17:05:54 -0400

    Prepared p5 canvas

---
+++ assets/js/scene.js
@@ -0,0 +1,11 @@
+function setup() {
+  var myCanvas = createCanvas(windowWidth, windowHeight);
+  frameRate(30);
+
+  myCanvas.style("display", "block");
+  myCanvas.parent("sceneHolder");
+}
+
+function draw() {
+
+}
