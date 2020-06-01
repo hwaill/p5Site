83704e552105f6d4acc39d1cbe8b05a659dd1f9f
Henry Waill <hwaill@optonline.net>
2020-06-01 17:57:05 -0400

    figured out editing canvas pixel array

--- assets/js/scene.js
+++ assets/js/scene.js
@@ -1,11 +1,44 @@
 function setup() {
-  var myCanvas = createCanvas(windowWidth, windowHeight);
+  pixelDensity(1);
+  createCanvas(windowWidth, windowHeight);
   frameRate(30);
 
-  myCanvas.style("display", "block");
-  myCanvas.parent("sceneHolder");
+  noLoop();
+
+  drawMountainBackground();
 }
 
 function draw() {
 
+}
+
+function drawMountainBackground() {
+  var colors = [
+    color('#fa6e8d'),
+    color('#e36b87'),
+    color('#cd6881'),
+    color('#b7657b'),
+    color('#a16175'),
+    color('#8a5d6f'),
+    color('#745869'),
+    color('#5e5363'),
+    color('#464e5e'),
+    color('#2a4858')
+  ];
+
+  loadPixels();
+  for (var y = 0; y < windowHeight; y++) {
+    for (var x = 0; x < windowWidth; x++) {
+      colorPixel(x, y, colors[0]);
+    }
+  }
+  updatePixels();
+}
+
+function colorPixel(x, y, color) {
+  var index = (x + y * windowWidth) * 4;
+  pixels[index] = red(color);
+  pixels[index + 1] = green(color);
+  pixels[index + 2] = blue(color);
+  pixels[index + 3] = 255;
 }
