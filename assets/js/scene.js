0ad9f44b1e738791c89a122b6752da02d6a83995
Henry Waill <hwaill@optonline.net>
2020-06-01 20:58:06 -0400

    Created leaf objects that fall and land on ground

--- assets/js/scene.js
+++ assets/js/scene.js
@@ -1,3 +1,35 @@
+var floorLevel;
+
+class Leaf {
+  constructor(startingPosition, diameter) {
+    this.position = startingPosition;
+    this.velocity = createVector(random(-1, 1), 4);
+    this.acceleration = createVector(-2, 4).limit(.083);
+
+    this.maxVelocity = 2;
+
+    this.diameter = diameter;
+    this.floorY = floorLevel + (random(20) - 10);
+
+    this.isOnFloor = false;
+  }
+
+  show() {
+    if (!this.isOnFloor) {
+      this.position.add(this.velocity.add(this.acceleration).limit(this.maxVelocity));
+      noStroke();
+      fill(color(255, 255, 255));
+      if (this.position.y > this.floorY) {
+        this.isOnFloor = true;
+      }
+    }
+    circle(this.position.x, this.position.y, this.diameter);
+  }
+}
+
+var mountainImage;
+var myLeaf;
+
 function setup() {
   pixelDensity(1);
   var myCanvas = createCanvas(windowWidth, windowHeight);
@@ -5,21 +37,24 @@ function setup() {
   myCanvas.parent("sceneHolder");
   frameRate(30);
 
-  noLoop();
+  floorLevel = windowHeight * 0.55;
 
-  drawMountainBackground();
+  createMountainImage();
+
+  myLeaf = new Leaf(createVector(windowWidth / 2, windowHeight / 3), 10);
 }
 
 function draw() {
-
+  image(mountainImage, 0, 0);
+  myLeaf.show();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  drawMountainBackground();
+  createMountainImage();
 }
 
-function drawMountainBackground() {
+function createMountainImage() {
   var colors = [
     color('#ae0d93'),
     color('#a6198f'),
@@ -35,32 +70,34 @@ function drawMountainBackground() {
     color('#2a4858')
   ];
 
-  loadPixels();
+  mountainImage = createImage(windowWidth, windowHeight);
+
+  mountainImage.loadPixels();
   for (var y = 0; y < windowHeight; y++) {
     for (var x = 0; x < windowWidth; x++) {
       colorPixel(x, y, colors[0]);
     }
   }
-  updatePixels();
+  mountainImage.updatePixels();
 
 
   var startingHeight;
   var thisHeight;
   for (var mountainLevel = 10; mountainLevel > 0; mountainLevel--) {
     var startingHeight = (Math.log10(mountainLevel) + 1) * 50;
-    loadPixels();
+    mountainImage.loadPixels();
     for (var imageColumn = 0; imageColumn < windowWidth; imageColumn++) {
       thisHeight = int((windowHeight / 2) - (startingHeight + (noise(mountainLevel, imageColumn * mountainLevel * 0.01) - 0.5) * 20));
       colorPixelsBelow(imageColumn, thisHeight, colors[11 - mountainLevel]);
     }
-    updatePixels();
+    mountainImage.updatePixels();
   }
 
-  loadPixels();
+  mountainImage.loadPixels();
   for (var col = 0; col < windowWidth; col++) {
     colorPixelsBelow(col, windowHeight / 2, colors[11]);
   }
-  updatePixels();
+  mountainImage.updatePixels();
 }
 
 function colorPixelsBelow(x, y, color) {
@@ -73,8 +110,8 @@ function colorPixelsBelow(x, y, color) {
 
 function colorPixel(x, y, color) {
   var index = (x + y * windowWidth) * 4;
-  pixels[index] = red(color);
-  pixels[index + 1] = green(color);
-  pixels[index + 2] = blue(color);
-  pixels[index + 3] = 255;
+  mountainImage.pixels[index] = red(color);
+  mountainImage.pixels[index + 1] = green(color);
+  mountainImage.pixels[index + 2] = blue(color);
+  mountainImage.pixels[index + 3] = 255;
 }
